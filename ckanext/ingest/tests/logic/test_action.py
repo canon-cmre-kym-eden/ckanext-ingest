c3d44fc386ccebe55d1a4eef71022db8a48a7d8d
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-03-05 20:26:17 +0200

    test import

--- ckanext/ingest/tests/logic/test_action.py
+++ ckanext/ingest/tests/logic/test_action.py
@@ -1,9 +1,11 @@
+
 import mimetypes
 import os
 from typing import Optional
 from werkzeug.datastructures import FileStorage
 import pytest
 from ckan.tests.helpers import call_action
+import ckan.plugins.toolkit as tk
 
 @pytest.fixture(scope="session")
 def source():
@@ -29,7 +31,29 @@ class TestExtractRecords:
         assert records == [{'name': 'hello', 'title': 'Hello', 'type': 'dataset'},
                            {'name': 'world', 'title': 'World', 'type': 'dataset'}]
 
+    def test_no_source(self, source):
+        with pytest.raises(tk.ValidationError):
+            call_action("ingest_extract_records")
+
     def test_unmapped(self, source):
         records = call_action("ingest_extract_records", source=source("unmapped.csv"))
         assert records == [{'type': 'dataset'},
                            {'type': 'dataset'}]
+
+
+
+@pytest.mark.usefixtures("clean_db")
+class TestImportRecords:
+    @pytest.mark.parametrize("filename", ["example.csv", "example.zip", "zipped_zip.zip"])
+    def test_basic(self, source, filename):
+        result = call_action("ingest_import_records", source=source(filename))
+        assert result == {"fail": 0, "success": 2}
+
+
+    def test_no_source(self, source):
+        with pytest.raises(tk.ValidationError):
+            call_action("ingest_import_records")
+
+    def test_unmapped(self, source):
+        result = call_action("ingest_import_records", source=source("unmapped.csv"))
+        assert result == {"fail": 2, "success": 0}
